7cbe04c64427f027eb07ef50a18004843ed6a75c
websiteh063-hue <websiteh063@gmail.com>
2026-07-29 09:50:35 +0530

    Set monitoring interval to every 2 minutes

--- monitor_daemon.py
+++ monitor_daemon.py
@@ -34,7 +34,7 @@ import time
 import traceback
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-INTERVAL = float(os.environ.get("PICKUP_INTERVAL", "180"))
+INTERVAL = float(os.environ.get("PICKUP_INTERVAL", "120"))
 MAX_UPTIME = float(os.environ.get("PICKUP_MAX_UPTIME", str(6 * 3600)))
 PUBLISH_SCRIPT = os.path.join(DIR, "publish_json.py")
 PUBLISH_LOG = os.path.join(DIR, "publish.log")
